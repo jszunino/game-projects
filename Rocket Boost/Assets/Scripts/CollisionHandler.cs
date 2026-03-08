834ba722fe1a9c94dca27b48e8c359c82624211a
Tekudo <mr.joaozunino@gmail.com>
2026-03-08 19:40:24 -0300

    feat(collision): add respawn and load next level

--- Rocket Boost/Assets/Scripts/CollisionHandler.cs
+++ Rocket Boost/Assets/Scripts/CollisionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -10,14 +11,28 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Its okay");
                 break;
             case "Obstacle":
+                SceneManager.LoadScene(0);
                 Debug.Log("-999 aura");
                 break;
             case "Finish":
+                LoadNextLevel();
                 Debug.Log("+999 aura");
                 break;
             default:
+                ReloadLevel();
                 Debug.Log("You're gay");
                 break;
         }
+    }
+
+    private void ReloadLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+    }
+    private void LoadNextLevel() 
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene + 1);
     }
 }
